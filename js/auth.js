a5fd5ed72eec4b675d4997be3ac24e7b59c21796
Grant Broadwater <grbcp5@mst.edu>
2018-07-03 11:33:45 -0500

    Basic authentication.

---
+++ js/auth.js
@@ -0,0 +1,69 @@
+/* global gapi */
+
+let API_KEY = "AIzaSyBlG0DgcKrot1t_nKo8n3cU20oDlZqmHD4";
+let CLIENT_ID = "438678561117-04vl6pt423vtr7dkmg5jc4mfbknna387.apps.googleusercontent.com";
+var GoogleAuth;
+let SCOPE = "https://www.googleapis.com/auth/drive.metadata.readonly";
+let standardMeteringURL = "localhost:63342";
+let standardMeteringAuthEndpoint = "/api/user/info";
+
+
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+
+  let discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
+
+  gapi.client.init( {
+    'apiKey': API_KEY,
+    'discoveryDocs': [discoveryUrl],
+    'clientId': CLIENT_ID,
+    'scope': SCOPE
+  } ).then(function() {
+    GoogleAuth = gapi.auth2.getAuthInstance();
+    GoogleAuth.isSignedIn.listen(updateSignInStatus);
+    GoogleAuth.signOut();
+  } );
+}
+
+function updateSignInStatus(isSignedIn) {
+  let user = GoogleAuth.currentUser.get();
+
+  if( user ) {
+    let id_token = user.getAuthResponse().id_token;
+    if( id_token ) {
+      authenticateIdToken(id_token);
+    } else {
+      GoogleAuth.signOut();
+    }
+  }
+}
+
+function authenticateIdToken(id_token) {
+
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', standardMeteringAuthEndpoint );
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.setRequestHeader("Authorization", "Bearer " + id_token);
+  xhr.addEventListener("load", handleAuthResponse);
+
+  console.log( "Authenticating google user." );
+  xhr.send();
+}
+
+function handleAuthResponse() {
+  jsonData = JSON.parse(this.responseText);
+  console.log(jsonData);
+
+  if(jsonData.accepted) {
+    $("#user_displayName").html(jsonData.data.display_name);
+  }
+}
+
+
+function signInButtonClicked() {
+    GoogleAuth.signIn();
+}
